142fa5e7b4e9d3f456f30ca65453df8cc0aa3bd9
Karni Wolf <karni@snyk.io>
2019-09-02 15:08:58 +0300

    add tests

--- exercises/container-security/docker-hub-cli/cli/index.js
+++ exercises/container-security/docker-hub-cli/cli/index.js
@@ -4,18 +4,20 @@ const args = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
 const dockerMetadata = require('../lib');
 
+module.exports = { validateDigestArgs, getDigest };
 
 const help = 'Usage: node cli/index.js --getDigest --repo=<repoName> --tag=<tagName>';
 
 if (args.help || args.h) {
   console.log(help);
-  return process.exit(0);
+  process.exit(0);
 }
 
 async function getDigest(repo, tag) {
   try {
     const digest = await dockerMetadata.getDockerContentDigest(repo, tag);
     console.log(chalk.green(`Fetched digest: ${digest} for ${repo}:${tag}`));
+    return digest;
   } catch (error) {
     if (error.statusCode === 401) {
       console.log(`Repo ${repo} does not exist on this Docker Hub registry account.`);
@@ -28,7 +30,6 @@ async function getDigest(repo, tag) {
 }
 
 function validateDigestArgs(args) {
-  console.log(args);
   if (typeof args.repo === 'string' && [ 'string', 'number' ].includes(typeof args.tag)) {
     return true;
   }
@@ -38,15 +39,19 @@ function validateDigestArgs(args) {
 }
 
 async function init() {
+  if (args.help || args.h) {
+    console.log(help);
+    return 0;
+  }
   if (args.getDigest) {
     if (!validateDigestArgs(args)) {
-      return process.exit(1);
+      return 1;
     }
     await getDigest(args.repo, args.tag);
   } else {
     console.log(help);
   }
-  return process.exit(0);
+  return 0;
 }
 
 init()
