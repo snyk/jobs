fe872459314288f79c5f1e25835152d2ca9a24c3
solal-2020 <sola.fitoussi@gmail.com>
2020-12-24 10:11:25 +0200

    Fetching child packages as part of the `/package` endpoint
    - Creating a recursive function to retrieve entire dependency branches.
    - Each cycle makes a network call.
    - Each cycle creates a number of new cycles based on the list of dependencies found.
    - The function returns when the "bottom dependency" is hit, meaning when we reach a node that does not have any dependency.

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 import got from 'got';
-import { NPMPackage } from './types';
+import { NPMPackage, Dependency } from './types';
+import { versionParser } from  './tools/versions';
 
 /**
  * Attempts to retrieve package data from the npm registry and return it
@@ -9,14 +10,53 @@ export const getPackage: RequestHandler = async function (req, res, next) {
   const { name, version } = req.params;
 
   try {
+    const dependencies = await getPackagesRecursively(name, version);
+    return res.status(200).json({
+      name: name,
+      version: version,
+      dependencies: dependencies,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+
+async function getPackagesRecursively(name, version) {
+/**
+ * The function calls itself recursively until it has reached the "bottom dependency".
+ * At which point, we return the entire dependency branch.
+ * Be careful: each cycle makes a network call
+ * and creates new cycles based on the list of packages found.
+ * @param  {string} name    - Name of the NPM package
+ * @param  {string} version - Desired version of the NPM package
+ * @return {Array.<Dependency>} A list of dependencies
+ */
+  try {
     const npmPackage: NPMPackage = await got(
       `https://registry.npmjs.org/${name}`,
     ).json();
 
     const dependencies = npmPackage.versions[version].dependencies;
+    let formattedDependencies = new Array();
 
-    return res.status(200).json({ name, version, dependencies });
-  } catch (error) {
-    return next(error);
+    if (dependencies !== undefined && Object.keys(dependencies).length > 0) {
+      for (let depName in dependencies) {
+        const depVersion = versionParser(dependencies[depName]),
+         subDependencies = await getPackagesRecursively(depName, depVersion);
+
+        const dependency: Dependency = {
+          name: depName,
+          version: depVersion,
+          dependencies: subDependencies,
+        };
+
+        formattedDependencies.push(dependency);
+      }
+    }
+
+    return formattedDependencies;
+  } catch(error) {
+    return error;
   }
-};
+}
