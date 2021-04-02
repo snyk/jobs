751ea643e00817754e4f7addd1540e8427c13e4b
Darrell Mozingo <darrell@mozingo.net>
2021-04-02 16:50:50 -0400

    Add and serve empy html page

--- exercises/npm-registry/src/app.ts
+++ exercises/npm-registry/src/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { getPackage } from './package';
+import * as path from 'path';
 
 /**
  * Bootstrap the application framework
@@ -10,6 +11,11 @@ export function createApp() {
   app.use(express.json());
 
   app.get('/package/:name/:version', getPackage);
+
+  app.get(['/', '/index.html'], function(req, res) {
+      // Running out of /dist, but static file isn't copied in there for some reason...
+      res.sendFile(path.join(__dirname + '/../index.html'));
+  });
 
   return app;
 }
