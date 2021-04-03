2aa5b96b28b8b828e7cf2be55c4cf2d07711e50a
Darrell Mozingo <darrell@mozingo.net>
2021-04-02 23:17:22 -0400

    Add simple in-memory cache of NPM responses
    Lots of issues, as detailed!

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -3,9 +3,20 @@ import got from 'got';
 import { getLatestSemVer, isSemVerFilter } from './semver';
 import { NPMPackage } from './types';
 
-/**
- * Attempts to retrieve package data from the npm registry and return it
- */
+/*
+  This in-memory Map is pretty dangerous:
+    - NPMPackage size & count are unbounded so will very likely consume available ram and crash the container
+    - not abiding by HTTP response cache headers, so updated NPM versions won't make it in here until the container restarts
+    - not shared by containers, so huge waste in resources as each has to cache the same entities
+    - new containers will create extra pressure on external resource (npm here) as the cache is empty, so a scaling event or redeploy means a stampeding herd :o
+
+  But it's also a few lines :)
+
+  Next steps would be an external in-line HTTP cache (eg Varnish). Could cluster it, serve stale responses if there's upstream issues, etc.
+
+  Depending on data mutation needs, cache hit rate, response sizes, etc I'd then consider an external memory cache (eg Redis/Memcache) if needed, then a persistant data store.
+*/
+const npmCache: Map<string, NPMPackage> = new Map<string, NPMPackage>();
 
 // METRIC histogram: req.package.client-time (from the client, be it UI or another backend service for a better overall view of latency)
 export const getPackage: RequestHandler = async function (req, res, next) {
@@ -26,25 +37,62 @@ export const getPackage: RequestHandler = async function (req, res, next) {
 async function getPackageDetails(deps: {[packageName: string]: string}): Promise<any[]> {
   return Promise.all(Object.keys(deps)
     .map(async name => {
-      // METRIC histogram: external.npm (success & failure separately)
-      // Optimise: in-memory Map, then an in-line HTTP cache (eg Varnish), then external memory cache (eg Redis/Memcache), then persisted dependending on data mutation needs, cache hit rate, response sizes, etc. Always respecting HTTP response cache headers from NPM to invalidate/update as needed.
-      // Optimise: run a local nock server to speed up tests and/or wrap & stub the calls in-memory so we're not hitting NPM all the time, reduce test time, and enable more edge case testing.
-      const npmPackage: NPMPackage = await got(
-        `https://registry.npmjs.org/${name}`,
-      ).json();
+      const npmPackage = await fetchNpmPackage(name);
 
       const requestedVersion = deps[name];
+      const version = determineVersion(requestedVersion, npmPackage);
+      const versionDef = npmPackage.versions[version];
 
-      let version = requestedVersion;
-      if (isSemVerFilter(requestedVersion)) {
-        // METRIC counter: req.package.semver-filter
-        const allVersions = Object.keys(npmPackage.versions);
-        version = getLatestSemVer(requestedVersion, allVersions);
+      if (!versionDef) {
+        // METRIC counter: req.package.version-not-found
+        throw new Error(`Version '${version} not found for package ${name}`);
       }
 
-      const dependencies = npmPackage.versions[version].dependencies;
+      const dependencies = versionDef.dependencies;
       const transitiveDeps = dependencies ? await getPackageDetails(dependencies) : undefined;
 
       return { name, version, dependencies, transitiveDeps };
     }));
+}
+
+function determineVersion(requestedVersion: string, npmPackage: NPMPackage) {
+  let version = requestedVersion;
+
+  if (isSemVerFilter(requestedVersion)) {
+    // METRIC counter: req.package.semver-filter
+    const allVersions = Object.keys(npmPackage.versions);
+    version = getLatestSemVer(requestedVersion, allVersions);
+  }
+
+  return version;
+}
+
+async function fetchNpmPackage(name: string): Promise<NPMPackage> {
+  // METRIC counter: external.npm.cache.attempt
+  let npmPackage = npmCache.get(name);
+
+  if (npmPackage) {
+    // METRIC counter: external.npm.cache.hit
+    return npmPackage;
+  }
+
+  // Optimise: run a local nock server to speed up tests and/or wrap & stub the calls in-memory so we're not hitting NPM all the time, reduce test time, and enable more edge case testing.
+
+  // METRIC counter: external.npm.cache.miss
+  // METRIC histogram: external.npm.time.call (tag success & failure separately)
+  // METRIC histogram: external.npm.time.deserialize (tag success & failure separately)
+  npmPackage = await got(
+    `https://registry.npmjs.org/${name}`
+  ).json();
+
+  if (!npmPackage) {
+    // METRIC counter: external.npm.fetch {status: failure}
+    throw new Error(`Error fetching or deserializing ${name} from NPM`);
+  }
+
+  // METRIC histogram: external.npm.cache.size.bytes (if bytes of npmPackage can be determined)
+  npmCache.set(name, npmPackage);
+  // METRIC histogram: external.npm.cache.size.count (npmCache.size())
+
+  return npmPackage;
 }
