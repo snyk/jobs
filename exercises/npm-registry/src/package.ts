c2569e3fb2338242908e5428a4e3bf715c58a0a6
Josh Emerson <josh@snyk.io>
2021-07-09 15:28:08 +0100

    feat: make npm-registry support latest version of package

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -1,17 +1,22 @@
-import { RequestHandler } from 'express';
-import got from 'got';
-import { NPMPackage } from './types';
+import { RequestHandler } from "express";
+import got from "got";
+import { NPMPackage } from "./types";
 
 /**
  * Attempts to retrieve package data from the npm registry and return it
  */
 export const getPackage: RequestHandler = async function (req, res, next) {
-  const { name, version } = req.params;
+  const { name } = req.params;
 
   try {
     const npmPackage: NPMPackage = await got(
-      `https://registry.npmjs.org/${name}`,
+      `https://registry.npmjs.org/${name}`
     ).json();
+
+    const version =
+      req.params.version === "latest"
+        ? npmPackage["dist-tags"].latest
+        : req.params.version;
 
     const dependencies = npmPackage.versions[version].dependencies;
 
