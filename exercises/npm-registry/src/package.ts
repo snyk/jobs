55df26c277412d0bdd14be7b082f4b38933c9e4e
Denise <dignat@yahoo.com>
2021-10-26 17:27:01 +0100

    pr review task

--- exercises/npm-registry/src/package.ts
+++ exercises/npm-registry/src/package.ts
@@ -11,14 +11,17 @@ export const getPackage: RequestHandler = async function (req, res, next) {
   const dependencyTree = {};
   try {
     const npmPackage: NPMPackage = await got(
-      `https://registry.npmjs.org/${name}`
+      `https://registry.npmjs.org/${name}`//good to be a constant
     ).json();
 
     const dependencies = npmPackage.versions[version].dependencies;
 
     for (const [dep, version] of Object.entries(dependencies as Object)) {
-      const subDep = await getDependencies(dep, version);
-      dependencyTree[dep] = { version, dependecies: subDep };
+      const subDep = await getDependencies(dep, version);// recursion inside a loop
+      // without a hard coded depth
+      // , or at least a logic where to stop drilling down is dangerous  may be tail-recursion optimization
+      // where minimum changes to the call stack are made -
+      dependencyTree[dep] = { version, dependencies: subDep };
     }
 
     return res
@@ -30,24 +33,32 @@ export const getPackage: RequestHandler = async function (req, res, next) {
 };
 
 async function getDependencies(name, version) {
-  return got(`https://registry.npmjs.org/${name}`)
+  const isNPMPackage = (value: unknown): value is NPMPackage => typeof value === 'object';
+  return  await got(`https://registry.npmjs.org/${name}`)
     .json()
-    .then((npmPackage: any) => {
-      let v = minSatisfying(Object.keys(npmPackage.versions), version);
+    .then((npmPackage: NPMPackage|any) => {//not ideal but NPMPackage type would error in Typescript
+      // 'Type 'unknown' is not assignable to type NPMPackage' we can change the ts config to not use strict check ,
+      // but it is not recommended, so check the api documentation to see what the return type is
+      // the return type is unknown , so we can do it some type guards
+      // by type assertion to make our intentions explicit
+      if (isNPMPackage(npmPackage)) {
+        let v = minSatisfying(Object.keys(npmPackage.versions), version);
 
-      if (v) {
-        const dependencyTree = {};
-        const newDeps = npmPackage.versions[v].dependencies;
-        for (const [dep, version] of Object.entries(
-          newDeps || ({} as Object)
-        )) {
-          const subDep = getDependencies(dep, version);
-          dependencyTree[dep] = { version, dependecies: subDep };
+        if (v) {
+          const dependencyTree = {};
+          const newDeps = npmPackage.versions[v].dependencies;
+          for (const [dep, version] of Object.entries(
+              newDeps || ({} as Object)
+          )) {
+            const subDep = getDependencies(dep, version);
+            dependencyTree[dep] = {version, dependencies: subDep};
+          }
+
+          return dependencyTree; // maybe is a good idea to make the dependencyTree a custom structure
+          // as we already know how the tree would look like
+        } else {
+          return {};
         }
-
-        return dependencyTree;
-      } else {
-        return {};
       }
     });
 }
